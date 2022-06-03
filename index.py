68f29afde7bb703d95eae3e8fbcf7838daeeae04
faishalmhmd <faishalmhmd4@gmail.com>
2022-06-03 18:38:18 +0700

    add: remove simbol & change to lowercase

--- index.py
+++ index.py
@@ -3,4 +3,10 @@ import csv
 with open('ulasan_tiket_com.csv','r') as data : 
     ulasan = csv.reader(data)
     for data in ulasan:
-        print(data[0])
+        # hapus nomor
+        remove_string = ''.join(filter(lambda item: not item.isdigit(), data[0]))
+        # hapus simbol (baru ;)
+        remove_char = remove_string.replace(';','')
+        # ubah ke non-kapital
+        lower_case = remove_char.lower()
+        print(lower_case)
