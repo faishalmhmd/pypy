bf5a08ef2778d38520838f82236a0b40e2e0e622
faishalmhmd <faishalmhmd4@gmail.com>
2022-06-04 06:01:22 +0700

    add: library Sastrawi & split word

--- index.py
+++ index.py
@@ -1,12 +1,23 @@
 import csv 
+import re
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# buat stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
 
 with open('ulasan_tiket_com.csv','r') as data : 
     ulasan = csv.reader(data)
     for data in ulasan:
         # hapus nomor
-        remove_string = ''.join(filter(lambda item: not item.isdigit(), data[0]))
+        pattern = r'[0-9]'
+        remove_string = re.sub(pattern,'',data[0])        
         # hapus simbol (baru ;)
         remove_char = remove_string.replace(';','')
         # ubah ke non-kapital
         lower_case = remove_char.lower()
-        print(lower_case)
+        #stemmer     
+        kata = stemmer.stem(lower_case)
+        # split jadi perkata
+        tiap_kata = kata.split()
+        print(tiap_kata)
