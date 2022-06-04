736727fe8cd4eefdd6e60ca467dbf171c5e26116
faishalmhmd <faishalmhmd4@gmail.com>
2022-06-04 20:51:30 +0700

    add : adding value to array 2D

--- index.py
+++ index.py
@@ -1,14 +1,17 @@
 import csv 
 import re
+import array
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
 # buat stemmer
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
+kumpulan_kata = []
+
 with open('ulasan_tiket_com.csv','r') as data : 
     ulasan = csv.reader(data)
-    for data in ulasan:
+    for idx,data in enumerate(ulasan):
         # hapus nomor
         pattern = r'[0-9]'
         remove_string = re.sub(pattern,'',data[0])        
@@ -19,5 +22,6 @@ with open('ulasan_tiket_com.csv','r') as data :
         #stemmer     
         kata = stemmer.stem(lower_case)
         # split jadi perkata
-        tiap_kata = kata.split()
-        print(tiap_kata)
+        kumpulan_kata.append([idx,kata])
+
+    print(*kumpulan_kata,sep='\n')
